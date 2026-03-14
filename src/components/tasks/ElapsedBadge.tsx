f95aae627c771579f71daec7decd1ac04910da5b
amiaoh <a.mia.oh@gmail.com>
2026-03-15 06:39:25 +1100

    Push elapsed badge to far right of actions row

--- src/components/tasks/ElapsedBadge.tsx
+++ src/components/tasks/ElapsedBadge.tsx
@@ -14,6 +14,7 @@ export function ElapsedBadge({ label }: ElapsedBadgeProps) {
       backgroundColor="whiteAlpha.300"
       borderRadius={4}
       paddingX={2}
+      ml="auto"
     >
       {label}
     </Text>
